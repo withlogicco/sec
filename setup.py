a469b58be1b380bc4367257913b3a47801f64650
Paris Kasidiaris <paris@sourcelair.com>
2018-04-12 15:44:51 +0300

    Update package information

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sec",
-    version="0.1.0",
+    version="0.2.0",
     description=(
         "Simple library for working with secrets in both files and "
         "environment variables."
@@ -10,6 +10,15 @@ setup(
     py_modules=["sec"],
     url="https://github.com/sourcelair/sec",
     author="Antonis Kalipetis, Paris Kasidiaris",
-    author_email="akalipetis@sourcelair.com, paris@sourcelair.com",
+    author_email="accounts@sourcelair.com",
     license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Secrets",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords='secrets development',
 )
